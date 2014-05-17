01cb64bf142b4da27467f7bcca40656065f6671e
Justin Moore <jmoore315@gmail.com>
2014-05-16 20:10:43 -0500

    game

--- etch_a_sketch_jquery/game.js
+++ etch_a_sketch_jquery/game.js
@@ -3,15 +3,29 @@ var boardsize = 840;
 
 
 $(document).ready( function() {
-	//$('#wrapper').width((boardsize/n) * n);
 	buildBoard(n);
 
 	$(document).on('mouseenter','.square', function() {
 		$(this).css('background-color','black');
 	});
+
+	$("#clear").click( function() {
+		$('.square').css('background-color','red');
+	});
+
+	$("#set_size").click( function() {
+		var num = prompt("Enter a number between 1 and 64");
+		if (num == parseInt(num) && num > 0 && num < 65){
+			buildBoard(num);
+			n = num;
+		}
+
+	});
+
 });
 
 function buildBoard(n){
+	$("#wrapper").empty();
 	for(j = 0; j < n; j++){
 		for(i = 0; i< n; i++) {
 			$('#wrapper').append("<div class='square'></div>");
@@ -30,11 +44,12 @@ function computeDimensions(n){
 	return boardsize/n - 2; //-2 for borders
 }
 
-$('#clear').click( function() {
+$("#clear").click( function() {
 	// $('#wrapper').empty();
 	$('.square').css('background-color','red');
 	// buildBoard(n);
 });
+
 
 function clearBoard() {
 	$('.square').css('background-color','red');
