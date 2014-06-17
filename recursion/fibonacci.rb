04a71b55192dc56d4fb13df061e41a213248cdb5
Justin Moore <jmoore315@gmail.com>
2014-06-16 22:19:08 -0500

    fibonacci

---
+++ recursion/fibonacci.rb
@@ -0,0 +1,33 @@
+
+def fib(n)
+  if n < 1 
+    return 0 
+  else 
+    current = 0
+    next_num = 1 
+    n.times do 
+      temp = next_num
+      next_num += current
+      current = temp 
+    end
+    current 
+  end
+end
+
+def fib_recursive(n) 
+  return 0 if n < 1
+  return 1 if n < 3
+  return fib_recursive(n-1) + fib_recursive(n-2)
+end
+
+puts "Fib(0) is: #{fib(0)} (should be 0)"
+puts "Fib(1) is: #{fib(1)} (should be 1)"
+puts "Fib(2) is: #{fib(2)} (should be 1)"
+puts "Fib(3) is: #{fib(3)} (should be 2)"
+puts "Fib(6) is: #{fib(6)} (should be 8)"
+
+puts "Fib_rec(0) is: #{fib_recursive(0)} (should be 0)"
+puts "Fib_rec(1) is: #{fib_recursive(1)} (should be 1)"
+puts "Fib_rec(2) is: #{fib_recursive(2)} (should be 1)"
+puts "Fib_rec(3) is: #{fib_recursive(3)} (should be 2)"
+puts "Fib_rec(6) is: #{fib_recursive(6)} (should be 8)"
